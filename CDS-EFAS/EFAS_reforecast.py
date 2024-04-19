fc23a7a880bff38559e44c90d61cc81b1bc7d4d5
Paolo Davini <p.davini@isac.cnr.it>
2024-04-19 17:17:49 +0200

    able to postprocess the ensembles

--- CDS-EFAS/EFAS_reforecast.py
+++ CDS-EFAS/EFAS_reforecast.py
@@ -7,13 +7,14 @@ import os
 import zipfile
 import cdsapi
 import pandas as pd
+import xarray as xr
 from cdo import *
 from pandas.tseries.offsets import Day
 cdo = Cdo()
 
 TGTDIR = "/work_big/users/davini/EFAS"
 TMPDIR = "/work_big/users/davini/EFAS/tmp"
-KIND = 'control' # 'control' or 'ensemble'
+KIND = 'ensemble' # 'control' or 'ensemble'
 VERSION = 4 # version of the EFAS reforecast, only 4 tested so far
 CLEAN = True # remove the temporary files
 
@@ -35,7 +36,10 @@ for date in total_date_range:
     day = date.strftime('%d')
     print(f"Downloading EFAS reforecast {KIND} for {year}-{month}-{day}")
 
-    target_file = f"{TGTDIR}/EFAS_reforecast_{year}{month}{day}_{KIND}.nc"
+    if KIND == 'control':
+        target_file = f"{TGTDIR}/EFAS_reforecast_{year}{month}{day}_{KIND}.nc"
+    elif KIND == 'ensemble':
+        target_file = f"{TGTDIR}/EFAS_reforecast_{year}{month}{day}_{KIND}_10.nc"  
     zip_file = f"{TMPDIR}/EFAS_reforecast_{year}{month}{day}_{KIND}.zip"
 
     request =  {
@@ -55,37 +59,60 @@ for date in total_date_range:
     elif KIND == 'ensemble':
         request['product_type'] = 'ensemble_perturbed_forecasts'
 
-    #print(request)
+    if os.path.exists(target_file):
+        print(f"File {target_file} already exists, skipping download")
+    else:
 
-    # make the request
-    if not os.path.exists(target_file):
-        print("Launcing the CDSAPI request...")
-        c = cdsapi.Client()
-        c.retrieve(
-            'efas-reforecast',
-            request,
-            zip_file)
-        
+        if os.path.exists(zip_file):
+            print(f"File {zip_file} already exists, skipping download")
+        else:
+            print("Launcing the CDSAPI request...")
+            c = cdsapi.Client()
+            c.retrieve(
+                'efas-reforecast',
+                request,
+                zip_file)
+            
         # Unzip the file
         print(f"Unzipping the file {zip_file}...")
         outdir = zip_file.replace('.zip', '')
         with zipfile.ZipFile(zip_file, 'r') as zip_ref:
             zip_ref.extractall(outdir)
-
-        # compress and set the time axis
-        print(f"Compressing and setting the time axis of the file {target_file}...")
-        cdo.settaxis(f'{year}-{month}-{day},00:00:00,6hour',
-                    input = f"-selname,dis06 {outdir}/mars_data_0.nc",
-                    output = target_file)
-        
         if CLEAN:
-            print("Cleaning up the temporary files...")
             os.remove(zip_file)
+
+        # this works only for the control
+        if KIND == 'control':
+            # compress and set the time axis
+            print(f"Compressing and setting the time axis of the file {target_file}...")
+            cdo.settaxis(f'{year}-{month}-{day},00:00:00,6hour',
+                        input = f"{outdir}/mars_data_0.nc",
+                        output = target_file,
+                        options = '-f nc4 -z zip')
+            
+        else:
+            print("Ensemble files not processable by CDO, splitting with Xarray")
+            dataset = xr.open_dataset(f"{outdir}/mars_data_0.nc")
+            for ensemble in dataset.number.values:
+                print(f"Ensemble {ensemble}")
+                ensname = f"{outdir}/mars_data_ens{ensemble}.nc"
+                dataset_ens = dataset.sel(number=ensemble).to_netcdf(ensname)
+                str_ensemble = str(ensemble).zfill(2)
+                target_file = f"{TGTDIR}/EFAS_reforecast_{year}{month}{day}_{KIND}_{str_ensemble}.nc"
+
+                cdo.settaxis(f'{year}-{month}-{day},00:00:00,6hour',
+                            input = ensname,
+                            output = target_file,
+                            options = '-f nc4 -z zip')
+                if CLEAN:
+                    os.remove(ensname)
+
+        if CLEAN:
             os.remove(f"{outdir}/mars_data_0.nc")
             os.rmdir(outdir)
+
     
-    else:
-        print(f"File {target_file} already exists, skipping download")
+
 
 
 
