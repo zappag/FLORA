22599a45aaa98d7667fd8a4f924dc088efa2fe49
Paolo Davini <p.davini@isac.cnr.it>
2024-09-18 06:55:12 +0200

    python slurm looping

--- CDS-EFAS/EFAS_seasonal.py
+++ CDS-EFAS/EFAS_seasonal.py
@@ -7,6 +7,7 @@ import os
 import zipfile
 import glob
 import logging
+import argparse
 import cdsapi
 import pandas as pd
 import xarray as xr
@@ -27,17 +28,32 @@ MASK = False # to mask everything
 os.makedirs(TGTDIR, exist_ok=True)
 os.makedirs(TMPDIR, exist_ok=True)
 
+
+# Set up command line argument parsing
+parser = argparse.ArgumentParser(description='EFAS reforecast downloader.')
+parser.add_argument('--year', type=int, required=True, help='The year to start the forecast (e.g. 2016)')
+parser.add_argument('--region', type=str, required=True, help='The region for which to download the reforecast (e.g. Panaro)')
+args = parser.parse_args()
+
+# Extract year and region from the command line arguments
+year = args.year
+region = args.region
+year1 = year + 2
+
 # create biweekly loop with pandas
-START_DATE = "1999-01-01"
-END_DATE = "2022-01-01"
+START_DATE = f"{year}-01-01"
+END_DATE = f"{year1}-01-01"
 BOXSIZE = .0 #how  many degrees do you want to extend the boundaries
 NENS = 25 # number of ensemble members
 DELTA = 24 # delta between the leadtimes in hours
 CHUNKS_DOWNLOAD = 215 # number of leadtimes to download at once
 MAXLEADTIME = 215*DELTA # maximum leadtime in hours of 215 days
 date_range = pd.date_range(start=START_DATE, end=END_DATE, freq='MS')
-REGIONS = ['Panaro', 'Timis', 'Lagen', 'Aragon', 'Reno']
 
+# Check if the region provided is in the predefined list
+REGIONS = ['Panaro', 'Timis', 'Lagen', 'Aragon', 'Reno']
+if region not in REGIONS:
+    raise ValueError(f"Region '{region}' is not a valid region. Choose from {REGIONS}")
 
 # loop over the dates
 for date in date_range:
@@ -46,101 +62,101 @@ for date in date_range:
     month = date.strftime('%m')
     day = date.strftime('%d')
     LAST = NENS - 1
-    for region in REGIONS:
-        target_file = f"{TGTDIR}/EFAS{VERSION}_reforecast_{region}_{year}{month}{day}_{KIND}_{LAST}.nc"
+    WRITEDIR = os.path.join(TGTDIR, region)
+    os.makedirs(WRITEDIR, exist_ok=True)
+    target_file = f"{WRITEDIR}/EFAS{VERSION}_reforecast_{region}_{year}{month}{day}_{KIND}_{LAST}.nc"
+    if os.path.exists(target_file):
+        print(f"File {target_file} already exists, skipping download")
+        continue
+
+    # full leadtime
+    logging.warning(f"Downloading EFAS{VERSION} reforecast {KIND} for {year}-{month}-{day} in {region}")
+    lat, lon = fixed_region(name=region, delta=BOXSIZE)
+    print(lat, lon)
+    steps = [str(i) for i in range(DELTA, MAXLEADTIME, DELTA)]
+    for chunk in chunks(steps, n=CHUNKS_DOWNLOAD):
+        target_file = f"{TMPDIR}/EFAS{VERSION}_reforecast__{region}_{year}{month}{day}_{KIND}_{chunk[0]}_{LAST}.nc"
         if os.path.exists(target_file):
-            print(f"File {target_file} already exists, skipping download")
+            logging.warning(f"File {target_file} already exists, skipping download")
             continue
-    
-        # full leadtime
-        logging.warning(f"Downloading EFAS{VERSION} reforecast {KIND} for {year}-{month}-{day} in {region}")
-        lat, lon = fixed_region(name=region, delta=BOXSIZE)
-        print(lat, lon)
-        steps = [str(i) for i in range(DELTA, MAXLEADTIME, DELTA)]
-        for chunk in chunks(steps, n=CHUNKS_DOWNLOAD):
-            target_file = f"{TMPDIR}/EFAS{VERSION}_reforecast__{region}_{year}{month}{day}_{KIND}_{chunk[0]}_{LAST}.nc"
-            if os.path.exists(target_file):
-                logging.warning(f"File {target_file} already exists, skipping download")
-                continue
 
-            zip_file = f"{TMPDIR}/EFAS_reforecast_{region}_{year}{month}{day}_{KIND}_{chunk[0]}.zip"
+        zip_file = f"{TMPDIR}/EFAS_reforecast_{region}_{year}{month}{day}_{KIND}_{chunk[0]}.zip"
 
-            # first check if the zip file is corrupted
-            if os.path.exists(zip_file):
-                try:
-                    the_zip_file = zipfile.ZipFile(zip_file)
-                except zipfile.BadZipFile:
-                    logging.error('Corrupted/incomplete zip file')
-                    os.remove(zip_file)
-
-            # download the file
-            if os.path.exists(zip_file):
-                logging.warning(f"File {zip_file} already exists, skipping download")
-            else:
-                request =  {
-                'data_format': 'netcdf4',
-                'download_format': 'zip',
-                'system_version': f'version_{VERSION}_0',
-                'variable': f'river_discharge_in_the_last_{DELTA}_hours',
-                'model_levels': 'surface_level',
-                'hyear': year,
-                'hmonth': month,
-                'leadtime_hour': chunk,
-                'area': [lat[1], lon[0], lat[0], lon[1]]
-                #'leadtime_hour': [str(i) for i in range(0, 5161, 24)]
-                    }
-                logging.warning("Launcing the CDSAPI request...")
-                logging.warning(request)
-                c = cdsapi.Client()
-                c.retrieve(
-                    'efas-seasonal-reforecast',
-                    request,
-                    zip_file)
-                
-            # Unzip the file
-            logging.warning(f"Unzipping the file {zip_file}...")
-            outdir = zip_file.replace('.zip', '')
-            with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-                zip_ref.extractall(outdir)
-                
-            logging.warning("Ensemble files not processable by CDO, splitting with Xarray")
-            dataset = xr.open_dataset(f"{outdir}/data_0.nc")
-            if MASK:
-                dataset = mask_efas(dataset, REGIONS)
-            for ensemble in dataset.number.values:
-                logging.warning(f"Ensemble {ensemble}")
-                ensname = f"{outdir}/mars_data_ens{ensemble}_{chunk[0]}.nc"
-                dataset_ens = dataset.sel(number=ensemble).to_netcdf(ensname)
-                str_ensemble = str(ensemble).zfill(2)
-                target_file = f"{TMPDIR}/EFAS{VERSION}_reforecast_{region}_{year}{month}{day}_{KIND}_{chunk[0]}_{str_ensemble}.nc"
-
-                delay = pd.Timedelta(int(chunk[0])/DELTA, unit="d")
-                reference_time = pd.Timestamp(f'{year}-{month}-{day}') + pd.Timedelta(delay, unit="d")
-                cdo.settaxis(f"{reference_time.strftime('%Y-%m-%d')},00:00:00,{DELTA}hours",
-                            input = ensname,
-                            output = target_file,
-                            options = '-f nc4 -z zip')
-                if CLEAN:
-                    os.remove(ensname)
-
-            if CLEAN:
-                os.remove(f"{outdir}/data_0.nc")
-                #os.rmdir(outdir)
+        # first check if the zip file is corrupted
+        if os.path.exists(zip_file):
+            try:
+                the_zip_file = zipfile.ZipFile(zip_file)
+            except zipfile.BadZipFile:
+                logging.error('Corrupted/incomplete zip file')
                 os.remove(zip_file)
-            
-        # merge the files
-        for ens in range(NENS):
-            logging.warning("Merging multiple for chunks for ensemble member %s", ens)
-            str_ensemble = str(ens).zfill(2)
-            files = f"{TMPDIR}/EFAS{VERSION}_reforecast_{region}_{year}{month}{day}_{KIND}_*_{str_ensemble}.nc"
-            WRITEDIR = os.path.join(TGTDIR, region)
-            os.makedirs(WRITEDIR, exist_ok=True)
-            cdo.merge(input = files, output = f"{WRITEDIR}/EFAS{VERSION}_reforecast_{region}_{year}{month}{day}_{KIND}_{str_ensemble}.nc")
-            if CLEAN:
-                for file in glob.glob(files):
-                    os.remove(file)
 
+        # download the file
+        if os.path.exists(zip_file):
+            logging.warning(f"File {zip_file} already exists, skipping download")
+        else:
+            request =  {
+            'data_format': 'netcdf4',
+            'download_format': 'zip',
+            'system_version': f'version_{VERSION}_0',
+            'variable': f'river_discharge_in_the_last_{DELTA}_hours',
+            'model_levels': 'surface_level',
+            'hyear': year,
+            'hmonth': month,
+            'leadtime_hour': chunk,
+            'area': [lat[1], lon[0], lat[0], lon[1]]
+            #'leadtime_hour': [str(i) for i in range(0, 5161, 24)]
+                }
+            logging.warning("Launcing the CDSAPI request...")
+            logging.warning(request)
+            c = cdsapi.Client()
+            c.retrieve(
+                'efas-seasonal-reforecast',
+                request,
+                zip_file)
+            
+        # Unzip the file
+        logging.warning(f"Unzipping the file {zip_file}...")
+        outdir = zip_file.replace('.zip', '')
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            zip_ref.extractall(outdir)
+            
+        logging.warning("Ensemble files not processable by CDO, splitting with Xarray")
+        netcdf_file = glob.glob(f"{outdir}/data*.nc")[0]
+        logging.warning(f"Unzipping the file {netcdf_file}...")
+        dataset = xr.open_dataset(netcdf_file)
+        if MASK:
+            dataset = mask_efas(dataset, REGIONS)
+        for ensemble in dataset.number.values:
+            logging.warning(f"Ensemble {ensemble}")
+            ensname = f"{outdir}/mars_data_ens{ensemble}_{chunk[0]}.nc"
+            dataset_ens = dataset.sel(number=ensemble).to_netcdf(ensname)
+            str_ensemble = str(ensemble).zfill(2)
+            target_file = f"{TMPDIR}/EFAS{VERSION}_reforecast_{region}_{year}{month}{day}_{KIND}_{chunk[0]}_{str_ensemble}.nc"
+
+            delay = pd.Timedelta(int(chunk[0])/DELTA, unit="d")
+            reference_time = pd.Timestamp(f'{year}-{month}-{day}') + pd.Timedelta(delay, unit="d")
+            cdo.settaxis(f"{reference_time.strftime('%Y-%m-%d')},00:00:00,{DELTA}hours",
+                        input = ensname,
+                        output = target_file,
+                        options = '-f nc4')
+            if CLEAN:
+                os.remove(ensname)
+
+        if CLEAN:
+            os.remove(netcdf_file)
+            #os.rmdir(outdir)
+            os.remove(zip_file)
         
+    # merge the files
+    for ens in range(NENS):
+        logging.warning("Merging multiple for chunks for ensemble member %s", ens)
+        str_ensemble = str(ens).zfill(2)
+        files = f"{TMPDIR}/EFAS{VERSION}_reforecast_{region}_{year}{month}{day}_{KIND}_*_{str_ensemble}.nc"
+        cdo.merge(input = files, output = f"{WRITEDIR}/EFAS{VERSION}_reforecast_{region}_{year}{month}{day}_{KIND}_{str_ensemble}.nc")
+        if CLEAN:
+            for file in glob.glob(files):
+                os.remove(file)
+
 
 
 
---
+++ CDS-EFAS/python_EFAS_slurm.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+# Define the list of years and regions to loop through
+years = range(2020, 2021)
+regions = ['Panaro', 'Timis', 'Lagen', 'Aragon', 'Reno']
+
+# SLURM submission script
+slurm_script = 'EFAS_seasonal.py'  # The SLURM script you created earlier
+
+# Loop through each year and region, submitting a job for each combination
+for year in years:
+    for region in regions:
+        # Create the sbatch command with the year and region as arguments
+        command = [
+            'sbatch',
+            f'--job-name=EFAS_{year}_{region}',            # Job name
+            f'--output=/home/davini/EFAS-{year}-{region}-%j.out', # Output file
+            f'--error=/home/davini/EFAS-{year}-{region}-%j.err',  # Error file
+            '--mem=4000M',                      # Memory
+            '--time=48:00:00',                  # Max run time
+            '--partition=batch',                # SLURM partition
+            '--wrap',                           # Wrap the Python command within SLURM
+            f'python EFAS_seasonal.py --year {year} --region {region}'  # Command to run
+        ]
+        
+        # Print the command to the terminal for debugging purposes
+        print(f"Submitting job for Year: {year}, Region: {region}")
+        
+        # Submit the job to SLURM
+        try:
+            result = subprocess.run(command, check=True, capture_output=True, text=True)
+            print(f"Job submitted successfully: {result.stdout}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error submitting job: {e.stderr}")
