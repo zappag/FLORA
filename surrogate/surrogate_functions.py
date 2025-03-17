3e1b205596924ec4b0f2a3b67841da295a3d6ce3
Paolo Davini <p.davini@isac.cnr.it>
2025-03-17 10:30:44 +0100

    surrogate docstring

--- surrogate/surrogate_functions.py
+++ surrogate/surrogate_functions.py
@@ -8,7 +8,17 @@ import pandas as pd
 
 
 def load_and_filter_file(filepath, discard_months, lead_months):
-    """Load a single NetCDF file, discard initial months, and select lead months."""
+    """
+    Load a single NetCDF file, discard initial months, and select lead months.
+
+    Args:
+        filepath (str): Path to the NetCDF file.
+        discard_months (int): Number of initial months to discard from the dataset.
+        lead_months (int): Number of lead months to select from the dataset after discarding initial months.
+    Returns:
+        xarray.Dataset or None: The filtered dataset containing the selected lead months, or None if no data is selected.
+    """
+
     ds = xr.open_dataset(filepath)
 
     # define a real time axis in numpy
@@ -40,12 +50,27 @@ def load_and_filter_file(filepath, discard_months, lead_months):
     return None
     
 def find_matching_files(date, mode, region, variable, ensemble_name, path):
+    """
+    Generate file path pattern and find matching files for a given date.
+
+    Args:
+        date (datetime): The date for which to find matching files.
+        mode (str): The mode of the forecast (e.g., 'SEAS5', 'EFAS5').
+        region (str): The region for which the forecast is generated.
+        variable (str): The variable of interest (e.g., '2m_temperature', 'total_precipitation').
+        ensemble_name (str): The name of the ensemble.
+        path (str): The base path where the files are stored.
+
+    Returns:
+        list: A list of file paths that match the generated pattern.
+
+    Raises:
+        FileNotFoundError: If no files matching the pattern are found.
+    """
     """Generate file path pattern and find matching files for a given date."""
     year = date.strftime('%Y')
     yearmonth = date.strftime('%Y%m')
     modenodigit = ''.join([i for i in mode if not i.isdigit()])
-    #if variable == 'dis24': #HACK for dis24
-    #    variable = ''
     file_pattern = os.path.join(path, region, year,
                                 f"{modenodigit}5_reforecast_{region}_{variable}_{yearmonth}01_{ensemble_name}.nc")
     print(file_pattern)
@@ -55,6 +80,21 @@ def find_matching_files(date, mode, region, variable, ensemble_name, path):
     return matched_files
 
 def target_filename(path, region, mode, variable, surrogate_kind, ensemble, offset_string):
+    """
+    Create a target filename structure based on the provided parameters.
+
+    Parameters:
+    path (str): The base directory path where the file will be saved.
+    region (str): The region identifier for the filename.
+    mode (str): The mode identifier, which may contain digits.
+    variable (str): The variable name to be included in the filename.
+    surrogate_kind (str): The type of surrogate to be included in the filename.
+    ensemble (str): The ensemble identifier for the filename.
+    offset_string (str): The offset string to be included in the directory and filename.
+
+    Returns:
+    str: The complete path to the target file with the constructed filename.
+    """
     """Create target filename structure"""
 
     final_directory = os.path.join(path, surrogate_kind, region, offset_string)
@@ -67,6 +107,20 @@ def target_filename(path, region, mode, variable, surrogate_kind, ensemble, offs
 
 
 def save_to_netcdf(dataset, filename):
+    """
+    Save a dataset to a NetCDF file with compression.
+
+    Parameters:
+    dataset (xarray.Dataset): The dataset to be saved.
+    filename (str): The path and name of the file to save the dataset to.
+
+    Returns:
+    None
+
+    Notes:
+    - The function applies zlib compression with a compression level of 1 to all variables in the dataset.
+    - The filename should include the .nc extension to indicate a NetCDF file.
+    """
     """Save NetCDF file"""
     # Extract the first timestep and get its month abbreviation
     #first_timestamp = dataset['forecast_period'].values[0]  # Assumes 'time' coordinate exists
@@ -82,7 +136,22 @@ def save_to_netcdf(dataset, filename):
     print(f"Data saved to {filename}")
 
 def get_surrogate_details(surrogate_kind, lead_months=7):
-    """Define discard_month, variants, and pd_delta based on surrogate_kind"""
+    """
+    Define discard_month, variants, and pd_delta based on surrogate_kind.
+
+    Parameters:
+    surrogate_kind (str): The type of surrogate, can be 'monthly', 'trimestral', or 'quadrimestral'.
+    lead_months (int, optional): The number of lead months. Default is 7.
+
+    Returns:
+    tuple: A tuple containing:
+        - discard_month (int): The number of months to discard based on the surrogate kind.
+        - variants (list): A list of integers representing the variants based on discard_month.
+        - pd_delta (str): A string representing the period delta in months.
+    
+    Raises:
+    ValueError: If an invalid surrogate_kind is provided.
+    """
 
     if surrogate_kind == 'monthly':
         discard_month = 1
